fdc0eae9b9db312f05949e3d58fc8694246c52e6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-20 16:22:04 +0100

    Added two more tests

--- test_space_x.py
+++ test_space_x.py
@@ -8,6 +8,32 @@ class TestSpaceX(unittest.TestCase):
 
         self.assertEqual(user.balance, 0)
 
+    def test_load_wallet(self):
+        user = User("Lone Voyager")
+        user.load_wallet(500)
+
+        self.assertEqual(user.balance, 500)
+
+    def test_balance_after_travels(self):
+        user = User("Lone Voyager")
+
+        abuja = Station("Abuja", "natural", "earth")
+        spock = Station("Spock", "natural", "mars")
+        iss = Station("ISS", "manmade", "earth")
+        moon = Station("Abuja", "natural", "earth")
+
+        falcon_1 = Rocket("Falcon 1", False)
+        falcon_9 = Rocket("Falcon 9", True)
+
+        # load the wallet with 3000BTC
+        user.load_wallet(3000)
+        # take trips
+        user.travel(abuja, moon, falcon_9)
+        user.travel(moon, spock, falcon_1)
+        user.travel(spock, iss, falcon_9)
+
+        self.assertEqual(user.balance, 1050)
+
 
 if __name__ == "__main__":
     unittest.main()
