a8a91b50b670e8496079b9f06d1a526042c37e70
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-20 16:08:54 +0100

    Test new users balance

--- test_space_x.py
+++ test_space_x.py
@@ -3,7 +3,10 @@ from space_x import User, Station, Rocket
 
 
 class TestSpaceX(unittest.TestCase):
-    pass
+    def test_new_user_balance(self):
+        user = User("Lone Voyager")
+
+        self.assertEqual(user.balance, 0)
 
 
 if __name__ == "__main__":
