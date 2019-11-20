4dc8b192a5729b803c14a480584f50bc608f4328
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-20 16:36:42 +0100

    Added some test to the main file

--- space_x.py
+++ space_x.py
@@ -46,3 +46,28 @@ class Rocket:
     def __init__(self, name, is_luxury):
         self.name = name
         self.is_luxury = is_luxury
+
+
+if __name__ == "__main__":
+    # instantiate the user
+    user = User("Lone Voyager")
+
+    # instantiate the stations
+    abuja = Station("Abuja", "natural", "earth")
+    spock = Station("Spock", "natural", "mars")
+    iss = Station("ISS", "manmade", "earth")
+    moon = Station("Abuja", "natural", "earth")
+
+    # instantiate the rockets
+    falcon_1 = Rocket("Falcon 1", False)
+    falcon_9 = Rocket("Falcon 9", True)
+
+    # load the wallet with 3000BTC
+    user.load_wallet(3000)
+    # take trips
+    user.travel(abuja, moon, falcon_9)
+    user.travel(moon, spock, falcon_1)
+    user.travel(spock, iss, falcon_9)
+
+    print(
+        f"After the trips, {user.name}'s balance is {user.wallet_balance}BTC")
